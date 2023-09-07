c9aef1e2aca0fbcd529ba3e6227a51084974669a
chaitu1969 <chaitanya.karle@gmail.com>
2023-09-07 14:28:54 +0530

    First Commit in ContactList

---
+++ Main.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const port = 9000;
+const path = require('path');
+
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
+// now below app var has app express functinalities
+const app = express();
+
+// setting template engine as ejs (viewEngine)
+app.set('view engine','ejs');
+//combining path where the ejs files are placed
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('assets'));
+
+// Creating a var list and adding key values
+// called the below list in below get request
+var Contactlist = [
+    {
+        name:"Tony",
+        Age:"50"
+    },
+    {
+        name:"Steve",
+        Age:"80"
+    },
+    {
+        name:"Thor",
+        Age:"1500"
+    }
+]
+
+app.get('/',function(req,res){
+    // console.log(res);
+    // res.send('<h1>server is running</h1>');
+
+    // console.log(res);
+
+    return res.render('home',{
+        title: "My List",
+        // calledthe created contact list as key value
+        Contact_List: Contactlist
+    });
+})
+
+app.post('/Updated_list',function(req,res){
+
+    // Contactlist.push({
+    //     name : req.body.name,
+    //     Age : req.body.Age
+    // });
+    // return res.redirect('/')
+
+    Contact.create({
+        name: req.body.Name,
+        Age : req.body.Age
+    }, function(err, newContact){
+        if(err){
+            console.log('error is generated while creating contact');
+            return ;
+        }
+        console.log("************", newContact);
+        return res.redirect('/');
+    });
+
+});
+
+//Rendering another Field Ejs file to JS
+
+
+app.get('/Field', function(req,res){
+    return res.render('Field',{
+        title: "BattleField"
+    })
+})
+
+app.get('/delete-contact',function(req,res){
+
+    console.log(req.query);
+
+    let Name = req.query.Name;
+
+    let contactIndex = Contactlist.findIndex(contact => contact.name == Name);
+    
+    
+    if(contactIndex != -1){
+        Contactlist.splice(contactIndex,1);
+    }
+
+    return res.redirect('back');
+})
+
+app.listen(port, function(err){
+    if(err){
+        console.log('got an error',err);
+        return 
+    }
+    console.log('server is up and running on ',port);
+    return;
+})
